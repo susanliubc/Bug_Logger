154cf55c9d2bbd0329f9defac7e581f2378f04bf
susanliubc <susanliubc@gmail.com>
2019-08-22 08:39:01 -0700

    add components folder

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
